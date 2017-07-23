16fba9409ab3ef7fc421f6f315b78c701091b98b
liujia <jiaLiu@CNjiaLiu.local>
2017-07-23 16:57:30 +0800

    [liujia]finish the secend practice of fac

--- src/Manualfac/02_should_create_named_object/src/Manualfac/Services/TypedService.cs
+++ src/Manualfac/02_should_create_named_object/src/Manualfac/Services/TypedService.cs
@@ -6,28 +6,41 @@ namespace Manualfac.Services
     {
         #region Please modify the following code to pass the test
 
+        Type type;
         /*
          * This class is used as a key for registration by type.
          */
 
         public TypedService(Type serviceType)
         {
-            throw new NotImplementedException();
+            this.type = serviceType;
         }
         
         public bool Equals(TypedService other)
         {
-            throw new NotImplementedException();
+            if (other == null)
+            {
+                return false;
+            }
+            return other.type == this.type;
         }
 
         public override bool Equals(object obj)
         {
-            throw new NotImplementedException();
+            if (obj == null)
+            {
+                return false;
+            }
+            if (obj is TypedService)
+            {
+                return this.Equals((TypedService) obj);
+            }
+            return false;
         }
 
         public override int GetHashCode()
         {
-            throw new NotImplementedException();
+            return this.type.GetHashCode();
         }
 
         #endregion
